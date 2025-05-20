30fb13192b2c99e02897cc24f865ea395c7f578a
Paumaro92 <paumr92@hotmail.es>
2025-05-20 11:13:29 +0200

    Añadido el Jordi de verdad.

--- s1_04_exercici1.php
+++ s1_04_exercici1.php
@@ -6,26 +6,33 @@ si ha de pagar o no impostos (si el sou supera 6000, paga impostos).
 Crea un objecte de la classe Employee i inicialitza’l amb el nom i sou corresponents. 
 Imprimeix el nom i si ha de pagar impostos o no.*/
 
-
 $employee = new Employee();
-$employee->initialize ("Pau" , 1500);
+$employee->initialize("Pau", 1500);
+$employee->print();
+$employee2 = new Employee();
+$employee2->initialize("Jordi", 7000);
+$employee2->print();
 
-class Employee {
-private $name;
-private $salary;
+class Employee
+{
+    private $name;
+    private $salary;
 
-public function initialize($name, $salary) {
-$this->name = $name;
-$this->salary = $salary;
+    public function initialize($name, $salary)
+    {
+        $this->name = $name;
+        $this->salary = $salary;
+    }
+
+    public function print()
+    {
+        echo "Nom: " . $this->name . "\n";
+        if ($this->salary > 6000) {
+            echo "Ha de pagar impostos.\n";
+        } else {
+            echo "No ha de pagar impostos.\n";
+        }
+    }
 }
 
-public function print() {
-echo "Nom: " . $this->name . "\n";
-if ($this->salary > 6000) {
-echo "Ha de pagar impostos.\n";
-} else {
-echo "No ha de pagar impostos.\n";
-}
-}
-}
 ?>
