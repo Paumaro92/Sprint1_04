e6e7e613a19f595c352a67e568a21f9867db07c4
Paumaro92 <paumr92@hotmail.es>
2025-05-20 11:17:51 +0200

    Quitado enunciado.

--- s1_04_exercici1.php
+++ s1_04_exercici1.php
@@ -1,10 +1,5 @@
 <?php
-/*Crea una classe Employee, defineix com a atributs el seu nom i sou. 
-Definir un mètode initialize que rebi com a paràmetres el nom i sou. 
-Plantejar un segon mètode print que imprimeixi el nom i un missatge 
-si ha de pagar o no impostos (si el sou supera 6000, paga impostos).
-Crea un objecte de la classe Employee i inicialitza’l amb el nom i sou corresponents. 
-Imprimeix el nom i si ha de pagar impostos o no.*/
+
 
 $employee = new Employee();
 $employee->initialize("Pau", 1500);
