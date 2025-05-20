ba057959b2cef22c2c9ee677e3725b3c592677b3
Paumaro92 <paumr92@hotmail.es>
2025-05-20 10:39:32 +0200

    No va.....no defino bien variable empleado

---
+++ s1_04_exercici1.php
@@ -0,0 +1,30 @@
+<?php
+/*Crea una classe Employee, defineix com a atributs el seu nom i sou. 
+Definir un mètode initialize que rebi com a paràmetres el nom i sou. 
+Plantejar un segon mètode print que imprimeixi el nom i un missatge 
+si ha de pagar o no impostos (si el sou supera 6000, paga impostos).
+Crea un objecte de la classe Employee i inicialitza’l amb el nom i sou corresponents. 
+Imprimeix el nom i si ha de pagar impostos o no.*/
+
+
+$employee1 ("Pau" , 1500);
+$employee2 ("Jordi" , 7000);
+class Employee {
+private $name;
+private $salary;
+
+public function initialize($name, $salary) {
+$this->name = $name;
+$this->salary = $salary;
+}
+
+public function print() {
+echo "Nom: " . $this->name . "\n";
+if ($this->salary > 6000) {
+echo "Ha de pagar impostos.\n";
+} else {
+echo "No ha de pagar impostos.\n";
+}
+}
+}
+?>
